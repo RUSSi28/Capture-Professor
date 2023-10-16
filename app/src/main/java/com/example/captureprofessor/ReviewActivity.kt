0c3ad3b3c5e7dce6736dba9553b3008cc76a6343
inukaki <inukaki8@gmail.com>
2023-10-16 14:01:23 +0900

    ソートを行う関数を別ファイルに整理しました。

--- app/src/main/java/com/example/captureprofessor/ReviewActivity.kt
+++ app/src/main/java/com/example/captureprofessor/ReviewActivity.kt
@@ -235,24 +235,3 @@ fun displayReview(review: Review) {
         }
     }
 }
-
-// enrollmentYearが遅い順にソート
-fun sortReviewByYear(initialReviews: MutableList<Review>): MutableList<Review> {
-    val sortedReviews = initialReviews.sortedByDescending { it.enrollmentYear }
-    val reviews = sortedReviews.toMutableList()
-    return reviews
-}
-
-// interestLevelが大きい順にソート
-fun sortReviewByInterest(initialReviews: MutableList<Review>): MutableList<Review> {
-    val sortedReviews = initialReviews.sortedByDescending { it.interestLevel }
-    val reviews = sortedReviews.toMutableList()
-    return reviews
-}
-
-// difficultyLevelが大きい順にソート
-fun sortReviewByDifficulty(initialReviews: MutableList<Review>): MutableList<Review> {
-    val sortedReviews = initialReviews.sortedByDescending { it.difficultyLevel }
-    val reviews = sortedReviews.toMutableList()
-    return reviews
-}
