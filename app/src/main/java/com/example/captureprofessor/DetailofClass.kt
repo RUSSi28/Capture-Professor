e636d89817cdb8e891a7f147f8277c9bdda6a6f6
RUSSi28 <110999990+RUSSi28@users.noreply.github.com>
2023-10-28 05:15:30 +0900

    fix: material3使用をmaterialいいｎ

--- app/src/main/java/com/example/captureprofessor/DetailofClass.kt
+++ app/src/main/java/com/example/captureprofessor/DetailofClass.kt
@@ -13,10 +13,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,10 +25,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.websarba.wings.android.detailofactivity.ui.theme.DetailOfActivityTheme
 
 @Composable
-fun DetailofClassUI(){
+fun DetailOfClassUI(){
     DetailPreview()
     ShowTitle("title")
 }
@@ -37,7 +36,7 @@ fun DetailofClassUI(){
 @Composable
 fun DetailPreview() {
     Column {
-        showTitle("授業名")
+        ShowTitle("授業名")
         Spacer(modifier = Modifier.padding(16.dp))
         ClassDetailWithButton()
     }
@@ -45,7 +44,7 @@ fun DetailPreview() {
 
 @Composable
 fun ShowTitle(title:String){
-    Surface(color=MaterialTheme.colorScheme.primary,modifier=Modifier.fillMaxWidth()) {
+    Surface(modifier=Modifier.fillMaxWidth()) {
         Text(
             text = "$title" ,modifier=Modifier.padding(12.dp)
         )
